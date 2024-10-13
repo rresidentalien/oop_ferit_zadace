4cfcb4dabd555e498f6d578394f9b50dd049fa53
rresidentalien <heyitslu.s@gmail.com>
2024-10-13 15:10:04 +0200

    feat(logic): class Episode created for dz1

---
+++ DZ1/tvshow/logic/Episode.cs
@@ -0,0 +1,43 @@
+﻿namespace logic
+{
+    public class Episode
+    {
+        private int viewerCount = 0;
+        private double sumOfScores = 0.0;
+        private double highestScore = 0.0;
+
+        public Episode() { }
+
+        public Episode(int viewerCount, double sumOfScores, double highestScore)
+        {
+            this.viewerCount = viewerCount;
+            this.sumOfScores = sumOfScores;
+            this.highestScore = highestScore;
+        }
+
+        public double GetMaxScore()
+        {
+            return highestScore;
+        }
+
+        public int GetViewerCount()
+        {
+            return viewerCount;
+        }
+
+        public double GetAverageScore()
+        {
+            return sumOfScores / viewerCount;
+        }
+
+        public void AddView(double score)
+        {
+            ++viewerCount;
+            sumOfScores += score;
+            if (score > highestScore)
+            {
+                highestScore = score;
+            }
+        }
+    }
+}
