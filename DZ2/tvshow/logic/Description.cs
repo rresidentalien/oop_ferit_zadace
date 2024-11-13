7eaf6e77c6344f9fdba7ebe0e9b8837a6b5431d0
rresidentalien <heyitslu.s@gmail.com>
2024-11-13 12:25:00 +0100

    feat(logic): created classes Description and TvUtilities

---
+++ DZ2/tvshow/logic/Description.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class Description
+    {
+        private int episodeNumber = 1;
+        private TimeSpan episodeLength = TimeSpan.FromMinutes(60);
+        private string episodeTitle = "";
+
+
+        public Description() { }
+
+        public Description(int episodeNumber, TimeSpan episodeLength, string episodeTitle)
+        {
+            this.episodeNumber = episodeNumber;
+            this.episodeLength = episodeLength;
+            this.episodeTitle = episodeTitle;
+        }
+
+
+    }
+}
---
+++ DZ2/tvshow/logic/TvUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class TvUtilities
+    {
+        private static Random random = new Random();
+
+
+        public static float GenerateRandomScore()
+        {
+            return (float)(random.NextDouble() * 10.0);
+        }
+
+        public static void Sort(Episode[] episodes)
+        {
+
+        }
+    }
+}
