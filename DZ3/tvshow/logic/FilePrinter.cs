311f1a3f45d9fd50f0e96c55227bea2c6c99677b
rresidentalien <heyitslu.s@gmail.com>
2024-12-10 12:03:15 +0100

    feat(logic): in progress - added new class Season and new interface IPrinter -> ConsolePrinter, FilePrinter

---
+++ DZ3/tvshow/logic/FilePrinter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class FilePrinter : IPrinter
+    {
+        private string outputFileName;
+
+        public FilePrinter(string outputFileName)
+        {
+            this.outputFileName = outputFileName;
+        }
+
+        public void Print(string message)
+        {
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(outputFileName, "WriteLines.txt")))
+            {
+                outputFile.WriteLine(message);
+            }
+        }
+
+
+    }
+}
