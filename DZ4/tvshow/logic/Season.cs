fef72501d7647a31b594b584cfe2c08b1ef3bbc0
rresidentalien <heyitslu.s@gmail.com>
2024-12-18 22:00:53 +0100

    refactor(season): changed to use list instead of array, added IEnumerable inheritance to be able to use foreach on season

---
+++ DZ4/tvshow/logic/Season.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class Season : IEnumerable<Episode>
+    {
+        private List<Episode> episodes;
+        private int seasonNumber;
+        public int Count { get => episodes.Count; }
+
+        public Season(int seasonNumber, List<Episode> episodes)
+        {
+            this.seasonNumber = seasonNumber;
+            this.episodes = episodes;
+        }
+
+        public Episode this[int i]
+        {
+            get => episodes[i];
+            set => episodes[i] = value;
+        }
+
+        public int GetTotalViewers()
+        {
+            int totalViewers = 0;
+            foreach (var episode in episodes)
+            {
+                totalViewers += episode.GetViewerCount();
+            }
+
+            return totalViewers;
+        }
+
+        public TimeSpan GetTotalTime()
+        {
+            TimeSpan totalTime = TimeSpan.FromSeconds(0);
+            foreach(var episode in episodes)
+            {
+                totalTime += episode.GetEpisodeLength();
+            }
+
+            return totalTime;
+        }
+
+        public DateTime GetBingeEnd()
+        {
+            return DateTime.Now + GetTotalTime();
+        }
+
+        public override string ToString()
+        {
+            string returnString = "";
+
+            returnString += $"Season {seasonNumber}:";
+            returnString += "\n=================================================\n";
+            foreach (var episode in episodes)
+            {
+                returnString += episode.ToString();
+                returnString += "\n";
+            }
+            returnString += "Report:";
+            returnString += "\n=================================================\n";
+            returnString += $"";
+            returnString += $"Total viewers: {GetTotalViewers()}\n";
+            returnString += $"Total duration: {GetTotalTime()}";
+            returnString += "\n=================================================\n";
+
+            return returnString;
+        }
+
+        public IEnumerator<Episode> GetEnumerator()
+        {
+            return episodes.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
