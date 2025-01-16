cc76ef63d453b5eeeaf9f7c3a4d8b110686b2d0c
rresidentalien <heyitslu.s@gmail.com>
2025-01-16 22:25:19 +0100

    chore: changed existing classes episode and season to work for json instead of user input

---
+++ DZ5/tvshow/core/Episode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class Episode
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int Runtime { get; set; }
+        public string Summary { get; set; }
+
+        private string RemoveTags(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+            return Regex.Replace(input, "<.*?>", string.Empty);
+        }
+
+        public override string ToString()
+        {
+            string cleanSummary = RemoveTags(Summary);
+            return $"{Number}: {Name}\n" +
+                $"Length: {Runtime} minutes\n" +
+                $"{cleanSummary}";
+        }
+    }
+}
---
+++ DZ5/tvshow/core/Season.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class Season
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int EpisodeOrder { get; set; }
+        public string PremiereDate { get; set; }
+        public string EndDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Season {Number}\n" +
+                $"Number of episodes: {EpisodeOrder}\n" +
+                $"Aired between {PremiereDate} and {EndDate}";
+        }
+    }
+}
